c78038a224e34ce525236d9ae08d0f53d5f0c485
Blake Han <byh2003@nyu.edu>
2024-04-10 19:42:43 -0400

    normalizer add default value

--- preprocessing_functions/column_processing.py
+++ preprocessing_functions/column_processing.py
@@ -6,7 +6,7 @@ from pyspark.ml.feature import VectorAssembler
 spark = SparkSession.builder.getOrCreate()
 
 
-def normalizer(df, column_name: str, column_new_name, range_start: float, range_end: float, min_max_scaling: bool = True, standardization: bool = True):
+def normalizer(df, column_name: str, column_new_name, range_start: float = 0, range_end: float = 1, min_max_scaling: bool = True, standardization: bool = True):
     if min_max_scaling:
         # Apply Min-Max Scaling
         min_max_scaler = MinMaxScaler(inputCol=column_name, outputCol=column_new_name, min=range_start, max=range_end)
