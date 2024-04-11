f8c98f8c0752fd0f05e201204fe9a3b699a863a0
sakakuGH <1831224479@qq.com>
2024-04-10 22:50:50 -0400

    Imputation function

--- preprocessing_functions/column_processing.py
+++ preprocessing_functions/column_processing.py
@@ -3,7 +3,8 @@ from pyspark.ml.feature import MinMaxScaler, StandardScaler
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import VectorAssembler
 from pyspark.sql import functions as F
-
+from pyspark.sql.types import NumericType
+from pyspark.sql.functions import col, isnan, when, desc
 spark = SparkSession.builder.getOrCreate()
 
 
@@ -44,3 +45,36 @@ def date_extraction(df, colname: str, new_colname: str, choice=None, anothercol=
     elif choice == 'weekday':
         output = df.withColumn(new_colname, F.lit('weekend' if F.dayofweek in [6, 7] else 'weekday'))
     return output
+
+
+def Imputation(df, threthold = 0.8, replace_strate = "mode_value"):
+    fields = df.schema.fields
+    for field in fields:
+        col_name = field.name
+        is_numerical = isinstance(field.dataType, NumericType)
+        drop_col = []
+        row_cnt = df.count()
+        cnt_NULL = df.select(col_name).where(col(col_name).isNull() | isnan(col(col_name))).count()
+        ratio = cnt_NULL / row_cnt
+        if ratio > threthold:
+            drop_col.append(col_name)
+        else:
+            if not is_numerical:
+                mode_value = df.select(col_name).filter(col(col_name).isNotNull() & (~isnan(col(col_name)))).orderBy(desc(col_name)).groupBy(col_name).count().first()[col_name]
+                df = df.withColumn(col_name, when(col(col_name).isNull() | isnan(col(col_name)), mode_value).otherwise(col(col_name)))
+            else:
+                if replace_strate = "mode_value":
+                    mode_value = df.select(col_name).filter(col(col_name).isNotNull() & (~isnan(col(col_name)))).orderBy(desc(col_name)).groupBy(col_name).count().first()[col_name]
+                    df = df.withColumn(col_name, when(col(col_name).isNull() | isnan(col(col_name)), mode_value).otherwise(col(col_name)))
+                elif replace_strate = "min_value":
+                    min_value = df.select(col_name).filter(col(col_name).isNotNull() & (~isnan(col(col_name)))).orderBy(col_name, ascending = True).first()[col_name]
+                    df = df.withColumn(col_name, when(col(col_name).isNull() | isnan(col(col_name)), min_value).otherwise(col(col_name)))
+                elif replace_strate = "max_value":
+                    max_value = df.select(col_name).filter(col(col_name).isNotNull() & (~isnan(col(col_name)))).orderBy(col_name, ascending = False).first()[col_name]
+                    df = df.withColumn(col_name, when(col(col_name).isNull() | isnan(col(col_name)), max_value).otherwise(col(col_name)))
+
+    df = df.drop(*drop_col)
+
+
+    return df
+
