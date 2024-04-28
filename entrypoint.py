2893168524fb807dde4c026c2ffae42f965e3145
Blake Han <byh2003@nyu.edu>
2024-04-28 19:17:39 -0400

    add Mike's

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,6 @@
 import json
 from pyspark.sql import SparkSession
-
+from profiling.column_profiling import column_textdata_profiler
 from tooling.open_ai import OPENAI
 
 # Create a SparkSession
@@ -16,15 +16,29 @@ df = spark.read.csv(
 
 #### DEMO part for normalizer
 # Show the DataFrame
-df.select("num_critic_for_reviews").show(3)
+# df.select("num_critic_for_reviews").show(3)
 
-# column normalizer
-from profiling.column_profiling import column_normalizer_profiler
+# # column normalizer
+# from profiling.column_profiling import column_normalizer_profiler
 
 
-client = OPENAI()
-df = column_normalizer_profiler(df, client)
+# client = OPENAI()
+# df = column_normalizer_profiler(df, client)
 
-df.select(
-    "num_critic_for_reviews_vector", "normalized_num_critic_for_reviews"
-).show(3)
+# df.select(
+#     "num_critic_for_reviews_vector", "normalized_num_critic_for_reviews"
+# ).show(3)
+
+
+def test_textdata_profiler(df):
+
+    # message = column_textdata_profiler(df)
+    # client = OPENAI()
+    # response = client.chat_completion(message, temperature=0)
+
+    # Convert JSON string to dictionary
+    result_df = column_textdata_profiler(df)
+    result_df.show()
+    return result_df
+
+test_textdata_profiler(df)
